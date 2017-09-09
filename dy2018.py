88e95dc1a24c6d5083edea4506aff8cae963f51c
LMX0809 <515540070@qq.com>
2017-09-09 16:03:39 +0800

    精简代码

--- dy2018.py
+++ dy2018.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3.5
+
 import requests #爬取模块
+import queue
 from bs4 import BeautifulSoup  #美味汤模块
 import re   #正则表达式模块
 from tkinter import *
 from tkinter import ttk
 import tkinter.scrolledtext
-import threading
-#print ("请输入爬取第____页")
-#p = input()
-#page = p
-#p = 1
-#try:
-#    if int(p) == 1:
+import time
+print ("dy2018spider starting")
+print ("wait 10s")
 url = 'http://www.dy2018.com/html/gndy/dyzz/index.html'
-#    else:
-#        url = 'http://www.dy2018.com/html/gndy/dyzz/index_' + page + '.html'
-#    print(url)
-#except:
-#    print("是第几页！！只能输入数字！一般第1页为最新。还不会的请发点红包给我,帮你充值一波智商")
+hd = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
 moive_url_list = [] #定义电影名称列表
 def get_info():  # 定义爬取函数
-    get_film = requests.get(url)#爬取网页
-    get_film.encoding = get_film.apparent_encoding #转换编码
+    try:
+        get_film = requests.get(url,headers = hd)#爬取网页
+        get_film.encoding = get_film.apparent_encoding#换编码
+    except:
+        print("connection timeout!check your network! please try again!")
     return get_film.text #传出函数
 demo = get_info() #调用并赋值
 def fill_info(): #定义调整信息函数
@@ -30,29 +26,26 @@ def fill_info(): #定义调整信息函数
     return fill_film #传出函数
 demo_fill = fill_info() #调用并赋值
 def son_info(): #定义获取最新网页中的子网页信息
-    for i in demo_fill:  #定义i在demo_fill中进行for循环
-        moive_url = 'http://www.dy2018.com' +i['href']#将遍历到的href接到url后面
-        son_film = requests.get(moive_url) #爬取网页
-        son_film.encoding = son_film.apparent_encoding#编码
-        son_moive_info = son_film.text#以文本形式赋值
-        son_soup = BeautifulSoup(son_moive_info,"html.parser")#转换为soup类
-        son_fill_film = son_soup.find_all('a') #过滤出a标签并赋值
-        for j in son_fill_film: #在sff中遍历
-            pa = re.compile(r'^ftp://.*.(mkv|rmvb)')#正则表达式匹配出下载链接
-            string = j['href']#当匹配到href就赋值
-            match = pa.match(string)#匹配字符串
-            if match:
-                moive_url_list.append(string) #增加字符串到列表中
-                moive_url_list.append('\n')#添加换行符，方便输出时不乱
-    return(moive_url_list)#传出列表
+    try:
+        for i in demo_fill:  #定义i在demo_fill中进行for循环
+            moive_url = 'http://www.dy2018.com' +i['href']#将遍历到的href接到url后面
+            son_film = requests.get(moive_url) #爬取网页
+            son_film.encoding = son_film.apparent_encoding#编码
+            son_moive_info = son_film.text#以文本形式赋值
+            son_soup = BeautifulSoup(son_moive_info,"html.parser")#转换为soup类
+            son_fill_film = son_soup.find_all('a') #过滤出a标签并赋值
+            for j in son_fill_film: #在sff中遍历
+                pa = re.compile(r'^ftp://.*.(mkv|rmvb)')#正则表达式匹配出下载链接
+                string = j['href']#当匹配到href就赋值
+                match = pa.match(string)#匹配字符串
+                if match:
+                    moive_url_list.append(string) #增加字符串到列表中
+                    moive_url_list.append('\n')#添加换行符，方便输出时不乱
+    except:
+        print("connection error please agine")
+    return (moive_url_list)  # 传出列表
 main = son_info()#调用函数
-
 def outp():#定义输出函数
-#    for i in main:
-#       print(i)
-#o = outp()
-#    var = main
-#    t.insert('insert',var)
     for var in main:
         t.insert('insert',var )
 root = Tk() #实例化变量名，方便调用
@@ -69,4 +62,4 @@ button.pack(side=BOTTOM,before=t) #设定按钮位置为底部，文本框之后
 label1=Label(root,text="developer：stone  QQ：515540070",fg='red'    )
 label1.pack(side=BOTTOM,before=button)
 root.mainloop() #创建一个基本窗口
-
+time.sleep(150)
